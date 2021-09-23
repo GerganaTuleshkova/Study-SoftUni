172c3752193f94c9740f4ed91d24f4978fe54f79
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2021-09-23 14:22:19 +0300

    Exercise files from lecture "Multidimensional Lists" - problem #07. Snake Moves

---
+++ 03_multidimensional_lists_excercises/07_snake_moves.py
@@ -0,0 +1,24 @@
+rows, columns = [int(x) for x in input().split()]
+matrix = []
+
+for row_index in range(rows):
+    row = [""] * columns
+    matrix.append(row)
+
+snake = input()
+
+
+count = 0
+for r_index in range(rows):
+    if r_index % 2 == 0:
+        for c_index in range(columns):
+            matrix[r_index][c_index] = snake[(count % len(snake))]
+            count += 1
+    else:
+        for c_index in range(columns - 1, -1, -1):
+            matrix[r_index][c_index] = snake[(count % len(snake))]
+            count += 1
+
+for row in matrix:
+    [print(i, end="") for i in row]
+    print()
