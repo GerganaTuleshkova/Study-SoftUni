933b3dce01899a1f049d680995ace95b8024516b
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2021-09-23 20:42:35 +0300

    second option

--- 03_multidimensional_lists_excercises/06_matrix_shuffling.py
+++ 03_multidimensional_lists_excercises/06_matrix_shuffling.py
@@ -10,6 +10,10 @@ def make_matrix():
     return matrix
 
 
+def is_valid_position(row, column, matrix):
+    return 0 <= row < len(matrix) and 0 <= column < len(matrix[0])
+
+
 matrix = make_matrix()
 
 while True:
@@ -21,23 +25,16 @@ while True:
     if len(command_info) != 5 or command_info[0] != "swap":
         print("Invalid input!")
         continue
-    else:
-        row1, col1, row2, col2 = [int(n) for n in command_info[1:]]
-        if \
-                (row1 > len(matrix) - 1) \
-                or (row2 > len(matrix) - 1) \
-                or (col1 > len(matrix[0]) - 1) \
-                or (col2 > len(matrix[0]) - 1)\
-                or (0 > row1) \
-                or (0 > row2) \
-                or (0 > col1) \
-                or (0 > col2):
-            print("Invalid input!")
-            continue
 
-        matrix[row1][col1], matrix[row2][col2] = matrix[row2][col2], matrix[row1][col1]
+    row1, col1, row2, col2 = [int(n) for n in command_info[1:]]
 
-        for row in matrix:
-            [print(i, end=" ") for i in row]
-            print()
+    if not is_valid_position(row1, col1, matrix) and not is_valid_position(row2, col2, matrix):
+        print("Invalid input!")
+        continue
+
+    matrix[row1][col1], matrix[row2][col2] = matrix[row2][col2], matrix[row1][col1]
+
+    for row in matrix:
+        [print(i, end=" ") for i in row]
+        print()
 
