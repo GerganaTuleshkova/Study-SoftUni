bc29cc2a2487cc6e59f7bfdd6732b92016582f30
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2021-09-24 09:54:26 +0300

    Solution for problem #02. Matrix Modification

---
+++ 03_multidimensional_lists_excercises_second/02_matrix_modification.py
@@ -0,0 +1,31 @@
+def is_valid_position(row_index, column_index, matrix):
+    return 0 <= row_index < len(matrix) and 0 <= column_index < len(matrix[0])
+
+
+n = int(input())
+
+matrix = []
+
+for _ in range(n):
+    row = [int(x) for x in input().split()]
+    matrix.append(row)
+
+command = input()
+
+while not command == "END":
+    command_args = command.split()
+    action = command_args[0]
+    row, column, value = [int(x) for x in command_args[1:]]
+    if not is_valid_position(row, column, matrix):
+        print("Invalid coordinates")
+
+    elif action == "Add" and is_valid_position(row, column, matrix):
+        matrix[row][column] += value
+    elif action == "Subtract" and is_valid_position(row, column, matrix):
+        matrix[row][column] -= value
+
+    command = input()
+
+for row in matrix:
+    [print(i, end=" ") for i in row]
+    print()
