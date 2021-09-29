c79a9d661304d6636443452da29309d30eab7a40
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2021-09-29 10:09:05 +0300

    modified

--- 04_functions_excercises/06_function_executor.py
+++ 04_functions_excercises/06_function_executor.py
@@ -1,6 +1,6 @@
-
 def sum_numbers(num1, num2):
     return num1 + num2
+
 
 def multiply_numbers(num1, num2):
     return num1 * num2
@@ -8,14 +8,10 @@ def multiply_numbers(num1, num2):
 
 def func_executor(*args):
     results = []
-    for i in args:
-        function = i[0]
-        parameters = i[1]
-
+    for function, parameters in args:
         current_result = function(*parameters)
         results.append(current_result)
     return results
-
 
 
 print(func_executor((sum_numbers, (1, 2)), (multiply_numbers, (2, 4))))
