0b5cb105bb75d76b21d4bdc2d87b2fd24d174ded
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2021-09-29 09:48:28 +0300

    modified

--- 04_functions_excercises/03_arguments_length.py
+++ 04_functions_excercises/03_arguments_length.py
@@ -1,9 +1,5 @@
 def args_length(*args):
-    count = 0
-    for argument in args:
-        count += 1
-
-    return count
+    return len(args)
 
 print(args_length(1, 32, 5))
 print(args_length("john", "peter"))
