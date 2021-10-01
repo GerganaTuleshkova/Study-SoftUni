ac3f511f26c13412ccf9f12b3b67d2a526490de0
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2021-10-01 19:38:25 +0300

    Solution fo problem # 01. Even Lines

---
+++ 05_file_handling_excercises/01_even_lines.py
@@ -0,0 +1,19 @@
+characters_to_replace = ["-", ",", ".", "!", "?"]
+
+file = open("text.txt", "r")
+
+count = 0
+
+for line in file:
+
+    if count % 2 == 0:
+        line = line.split()[:: -1]
+        line = " ".join(line)
+        line = list(line)
+        for i in range(len(line)):
+            if line[i] in characters_to_replace:
+                line[i] = "@"
+
+        print("".join(line))
+
+    count += 1
